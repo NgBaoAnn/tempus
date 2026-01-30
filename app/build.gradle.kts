4c00d8ecc9f864b06ae05f29641f92e21b143d70
VNSHuy24 <hvo6471@gmail.com>
2026-01-30 14:36:25 +0700

    fix gg-auth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,15 @@ android {
         compose = true
     }
 
+    signingConfigs {
+        create("shared") {
+            storeFile = file("keystore/debug.keystore")
+            storePassword = "android"
+            keyAlias = "AndroidDebugKey"
+            keyPassword = "android"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -53,6 +62,9 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+        debug {
+            signingConfig = signingConfigs.getByName("shared")
         }
     }
     compileOptions {
